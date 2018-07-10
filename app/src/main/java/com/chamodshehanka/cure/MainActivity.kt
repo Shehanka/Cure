19077f2a7eed1cd6da83844fb70b6aa34deb404c
Shehanka <hcsperera@outlook.com>
2018-07-10 21:34:42 +0530

    Making login action

--- app/src/main/java/com/chamodshehanka/cure/MainActivity.kt
+++ app/src/main/java/com/chamodshehanka/cure/MainActivity.kt
@@ -2,6 +2,8 @@ package com.chamodshehanka.cure
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import java.net.PasswordAuthentication
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun emailLogin(userEmail: ContactsContract.CommonDataKinds.Email, userPasswordAuthentication: PasswordAuthentication) {
+
     }
 }
